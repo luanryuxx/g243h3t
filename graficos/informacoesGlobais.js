19fdba03ac98133104efcd793c88efd0866b433f
luanryux <88515809+luanryuxx@users.noreply.github.com>
2024-09-30 11:39:54 -0300

    Delete graficos directory

--- graficos/informacoesGlobais.js
+++
@@ -1,7 +0,0 @@
-const url = "https://raw.githubusercontent.com/"guilhermeonrails/api/main/dados-globalThis.json";
-
-async function vizualizarInformacoesGlobais(){
-    const res = await fetch(url);
-    const dados = res.json();
-    
-}
