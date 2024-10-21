a0bdd81682259dedbbbc8d004a2a6648322504a8
luanryux <88515809+luanryuxx@users.noreply.github.com>
2024-10-21 12:31:00 -0300

    Add files via upload

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -1,4 +1,6 @@
-async function quantidadeUsuarios() {
+import { getCSS, tickConfig, criarGrafico } from "./common.js"
+
+async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
@@ -16,22 +18,22 @@ async function quantidadeUsuarios() {
         }
     ]
 
-    const laytout = {
+    const layout = {
         plot_bgcolor: getCSS('--bg-color'),
         paper_bgcolor: getCSS('--bg-color'),
         title: {
-            text: 'Redes sociais com mais usuários',
+            text: 'Redes sociais com mais usuários no mundo',
             x: 0,
             font: {
                 color: getCSS('--primary-color'),
-                size: 30,
-                font: getCSS('--font')
+                family: getCSS('--font'),
+                size: 30
             }
         },
         xaxis: {
             tickfont: tickConfig,
             title: {
-                text: 'Nome das redes',
+                text: 'nome das redes sociais',
                 font: {
                     color: getCSS('--secondary-color')
                 }
@@ -40,18 +42,16 @@ async function quantidadeUsuarios() {
         yaxis: {
             tickfont: tickConfig,
             title: {
-                text: 'Bilhões de usuários ativos',
+                text: 'bilhões de usuários ativos',
                 font: {
                     color: getCSS('--secondary-color')
                 }
             }
         }
+
     }
 
-    const grafico = document.createElement('div')
-    grafico.className = 'grafico'
-    document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data, laytout)
+    criarGrafico(data, layout)
 }
 
-quantidadeUsuarios()
+quantidadeUsuariosPorRede()
